9282335cc920f1325075da9fd8eac08db385efc9
Bjarke Sørensen <bs@wasd.dk>
2016-12-12 23:43:50 +0100

    Added password fields on entering data

--- src/main.rs
+++ src/main.rs
@@ -12,17 +12,17 @@ use pencil::method::Post;
 use pencil::HTTPError;
 use std::collections::BTreeMap;
 use rustc_serialize::json::{Json, ToJson};
-//use time::Timespec;
+// use time::Timespec;
 use rusqlite::Connection;
 
 const BETA: f64 = 5000.0;
 
 #[derive(Debug)]
 struct PlayedGame {
-    home:   String,
-    away:   String,
-    home_score:   i32,
-    away_score:  i32,
+    home: String,
+    away: String,
+    home_score: i32,
+    away_score: i32,
 }
 
 impl ToJson for PlayedGame {
@@ -46,11 +46,14 @@ fn newgame_con() -> BTreeMap<String, Json> {
     let conn = Connection::open("ratings.db").unwrap();
     let mut stmt = conn.prepare("SELECT id, name FROM players").unwrap();
     let mut names: Vec<_> = stmt.query_map(&[], |row| {
-        let mut n = BTreeMap::new();
-        n.insert("id".to_string(), row.get::<_, i32>(0).to_json());
-        n.insert("name".to_string(), row.get::<_, String>(1).to_json());
-        n.to_json()
-    }).unwrap().map(Result::unwrap).collect();
+            let mut n = BTreeMap::new();
+            n.insert("id".to_string(), row.get::<_, i32>(0).to_json());
+            n.insert("name".to_string(), row.get::<_, String>(1).to_json());
+            n.to_json()
+        })
+        .unwrap()
+        .map(Result::unwrap)
+        .collect();
 
     names.sort_by(|_, _| *rand::thread_rng().choose(&[Greater, Less]).unwrap());
 
@@ -71,32 +74,54 @@ fn submit_newgame(request: &mut Request) -> PencilResult {
     let home_score: i32 = request.form().get("home_score").unwrap().parse().unwrap();
     let away_score: i32 = request.form().get("away_score").unwrap().parse().unwrap();
 
+    let secret = request.form().get("secret").unwrap();
+    if secret != "jeg er sikker" {
+        let mut context = newgame_con();
+        context.insert("nykamp_fejl".to_string(),
+                       "Det indtastede kodeord er forkert 💩".to_json());
+        context.insert("heading".to_string(), "Fejl: Ny kamp".to_json());
+        context.insert("body".to_string(),
+                       "Fejl ved registrering af ny kamp".to_json());
+        return request.app.render_template("index.html", &context);
+    }
+
     if !(home_score == 10 || away_score == 10) || home_score == away_score || home == away {
         let mut context = newgame_con();
 
-        context.insert("nykamp_fejl".to_string(), "Den indtastede kamp er ikke lovlig 😜".to_json());
+        context.insert("nykamp_fejl".to_string(),
+                       "Den indtastede kamp er ikke lovlig 😜".to_json());
         context.insert("heading".to_string(), "Fejl: Ny kamp".to_json());
-        context.insert("body".to_string(), "Fejl ved registrering af ny kamp".to_json());
-        return request.app.render_template("index.html", &context)
+        context.insert("body".to_string(),
+                       "Fejl ved registrering af ny kamp".to_json());
+        return request.app.render_template("index.html", &context);
     }
 
-    println!("{:?}", conn.execute("INSERT INTO games (home_id, away_id, home_score, away_score) VALUES (?, ?, ?, ?)",
-        &[&home, &away, &home_score, &away_score]));
+    println!("{:?}",
+             conn.execute("INSERT INTO games (home_id, away_id, home_score, away_score) VALUES \
+                           (?, ?, ?, ?)",
+                          &[&home, &away, &home_score, &away_score]));
 
     pencil::redirect("/", 302)
 }
 
 fn games(request: &mut Request) -> PencilResult {
     let conn = Connection::open("ratings.db").unwrap();
-    let mut stmt = conn.prepare("SELECT (SELECT name FROM players p WHERE p.id = g.home_id) AS home, (SELECT name FROM players p WHERE p.id = g.away_id) AS away, home_score, away_score FROM games g ORDER BY ID DESC").unwrap();
-    let games: Vec<_> = stmt.query_map(&[], |row|
-        PlayedGame {
-            home: row.get(0),
-            away: row.get(1),
-            home_score: row.get(2),
-            away_score: row.get(3),
-        }
-    ).unwrap().map(Result::unwrap).collect();
+    let mut stmt =
+        conn.prepare("SELECT (SELECT name FROM players p WHERE p.id = g.home_id) AS home, \
+                      (SELECT name FROM players p WHERE p.id = g.away_id) AS away, home_score, \
+                      away_score FROM games g ORDER BY ID DESC")
+            .unwrap();
+    let games: Vec<_> = stmt.query_map(&[], |row| {
+            PlayedGame {
+                home: row.get(0),
+                away: row.get(1),
+                home_score: row.get(2),
+                away_score: row.get(3),
+            }
+        })
+        .unwrap()
+        .map(Result::unwrap)
+        .collect();
 
     let mut context = BTreeMap::new();
 
@@ -118,32 +143,32 @@ struct Player {
     rating: Rating,
     kampe: u32,
     vundne: u32,
-    tabte: u32
+    tabte: u32,
 }
 
 use std::mem::replace;
 use bbt::Outcome::{Win, Loss};
 
 impl Player {
-    fn new<S: ToString>(name: S) -> Self{
-        Player{
+    fn new<S: ToString>(name: S) -> Self {
+        Player {
             name: name.to_string(),
-            rating: Rating::new(BETA, BETA/3.0),
+            rating: Rating::new(BETA, BETA / 3.0),
             kampe: 0,
             vundne: 0,
-            tabte: 0
+            tabte: 0,
         }
     }
     fn duel(&mut self, rater: &Rater, o: Rating, won: bool) {
         let a = replace(&mut self.rating, Default::default());
 
-        let (a, _) = rater.duel(a, o, if won{Win}else{Loss});
+        let (a, _) = rater.duel(a, o, if won { Win } else { Loss });
         self.rating = a;
 
         self.kampe += 1;
-        if won{
+        if won {
             self.vundne += 1;
-        }else{
+        } else {
             self.tabte += 1;
         }
     }
@@ -152,8 +177,10 @@ impl Player {
 impl ToJson for Player {
     fn to_json(&self) -> Json {
         let mut m: BTreeMap<String, Json> = BTreeMap::new();
-        m.insert("rating".to_string(), format!("{:.1}", self.rating.mu()).to_json());
-        m.insert("sigma".to_string(), format!("{:.1}", self.rating.sigma()).to_json());
+        m.insert("rating".to_string(),
+                 format!("{:.1}", self.rating.mu()).to_json());
+        m.insert("sigma".to_string(),
+                 format!("{:.1}", self.rating.sigma()).to_json());
         m.insert("name".to_string(), self.name.to_json());
         m.insert("kampe".to_string(), self.kampe.to_json());
         m.insert("vundne".to_string(), self.vundne.to_json());
@@ -171,18 +198,19 @@ fn get_rating(rating: &Rating) -> f64 {
 struct Game {
     home: i32,
     away: i32,
-    home_win: bool
+    home_win: bool,
 }
 
 use std::cmp::Ordering::{Greater, Less};
 use std::collections::HashMap;
 
 fn rating(request: &mut Request) -> PencilResult {
-    let rater = Rater::new(BETA/6.0);
+    let rater = Rater::new(BETA / 6.0);
 
     let conn = Connection::open("ratings.db").unwrap();
     let mut stmt = conn.prepare("SELECT id, name from players").unwrap();
-    let mut stmt2 = conn.prepare("SELECT home_id, away_id, home_score, away_score from games").unwrap();
+    let mut stmt2 = conn.prepare("SELECT home_id, away_id, home_score, away_score from games")
+        .unwrap();
 
     let mut players = HashMap::new();
 
@@ -191,11 +219,14 @@ fn rating(request: &mut Request) -> PencilResult {
         players.insert(id, Player::new(name));
     }
 
-    for g in stmt2.query_map(&[], |row| Game{
-        home: row.get(0),
-        away: row.get(1),
-        home_win: row.get::<_, i32>(2) > row.get::<_, i32>(3)
-    }).unwrap() {
+    for g in stmt2.query_map(&[], |row| {
+            Game {
+                home: row.get(0),
+                away: row.get(1),
+                home_win: row.get::<_, i32>(2) > row.get::<_, i32>(3),
+            }
+        })
+        .unwrap() {
         let g = g.unwrap();
         let away_rating = players[&g.away].rating.clone();
         let home_rating = players[&g.home].rating.clone();
@@ -204,7 +235,11 @@ fn rating(request: &mut Request) -> PencilResult {
     }
 
     let mut ps: Vec<_> = players.values().map(Clone::clone).collect();
-    ps.sort_by(|a, b| if get_rating(&b.rating) < get_rating(&a.rating){Less}else{Greater});
+    ps.sort_by(|a, b| if get_rating(&b.rating) < get_rating(&a.rating) {
+        Less
+    } else {
+        Greater
+    });
     ps.insert(0, Player::new("N/A"));
 
     let mut context = BTreeMap::new();
@@ -250,20 +285,32 @@ fn newplayer_con() -> BTreeMap<String, Json> {
 }
 
 fn submit_newplayer(request: &mut Request) -> PencilResult {
-    let conn = Connection::open("ratings.db").unwrap();
-
+    {
+        let secret = request.form().get("secret").unwrap();
+        if secret != "jeg er sikker" {
+            let mut context = newplayer_con();
+            context.insert("nyspiller_fejl".to_string(),
+                           "Det indtastede kodeord er forkert 💩".to_json());
+            context.insert("heading".to_string(), "Fejl: Ny spiller".to_json());
+            context.insert("body".to_string(),
+                           "Fejl ved registrering af ny spiller".to_json());
+            return request.app.render_template("index.html", &context);
+        }
+    }
     let name = request.form().get("name").unwrap();
-
     if name.is_empty() {
         let mut context = newplayer_con();
 
-        context.insert("nyspiller_fejl".to_string(), "Den indtastede spiller er ikke lovlig 😜".to_json());
+        context.insert("nyspiller_fejl".to_string(),
+                       "Den indtastede spiller er ikke lovlig 😜".to_json());
         context.insert("heading".to_string(), "Fejl: Ny spiller".to_json());
-        context.insert("body".to_string(), "Fejl ved registrering af ny spiller".to_json());
-        return request.app.render_template("index.html", &context)
+        context.insert("body".to_string(),
+                       "Fejl ved registrering af ny spiller".to_json());
+        return request.app.render_template("index.html", &context);
     }
-    println!("{:?}", conn.execute("INSERT INTO players (name) VALUES (?)",
-        &[&*name]));
+    let conn = Connection::open("ratings.db").unwrap();
+    println!("{:?}",
+             conn.execute("INSERT INTO players (name) VALUES (?)", &[&*name]));
 
     pencil::redirect("/", 302)
 }
@@ -279,7 +326,10 @@ fn main() {
     app.get("/players", "players hest", players);
     app.route("/newgame/submit", &[Post], "ny kamp", submit_newgame);
     app.get("/newplayer", "players new new new hest", newplayer);
-    app.route("/newplayer/submit", &[Post], "ny hestesandwcih", submit_newplayer);
+    app.route("/newplayer/submit",
+              &[Post],
+              "ny hestesandwcih",
+              submit_newplayer);
 
     app.enable_static_file_handling();
     app.httperrorhandler(404, page_not_found);
